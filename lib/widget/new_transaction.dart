dc56a326f0796c557e5571fdc620797146770668
Junosoft <junosoftltd@gmail.com>
2023-02-16 10:14:07 +0200

    added files

---
+++ lib/widget/new_transaction.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class NewTrsansaction extends StatefulWidget {
+  const NewTrsansaction({super.key});
+
+  @override
+  State<NewTrsansaction> createState() => _NewTrsansactionState();
+}
+
+class _NewTrsansactionState extends State<NewTrsansaction> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/widget/user_transactions.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class UserTransactions extends StatefulWidget {
+  const UserTransactions({super.key});
+
+  @override
+  State<UserTransactions> createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
