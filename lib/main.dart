44114068f6102bf6607a30e504f5ad5d94012213
Junosoft <junosoftltd@gmail.com>
2023-02-16 10:08:45 +0200

    updated but partial complete step

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import './model/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import './widget/transaction_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,12 +23,7 @@ class MyApp extends StatelessWidget {
 }
 
 class _MyHomePage extends StatelessWidget {
-  final List<Transaction> transaction = [
-    Transaction(
-        amount: 68.99, date: DateTime.now(), id: 'X5667', title: 'New Shoes'),
-    Transaction(
-        amount: 68.99, date: DateTime.now(), id: 'X5667', title: 'New Car'),
-  ];
+  
   final titleController = TextEditingController();
   final amountController = TextEditingController();
   @override
@@ -75,45 +71,7 @@ class _MyHomePage extends StatelessWidget {
                     ],
                   ),
                 )),
-            Column(
-                children: transaction.map((tx) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 8, horizontal: 10),
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.purple,
-                        width: 3,
-                      )),
-                      child: Text('\$ ${tx.amount}'),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            child: Text(
-                              tx.title,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 19,
-                                  color: Color.fromARGB(255, 124, 77, 1)),
-                            )),
-                        Text(
-                          DateFormat.yMMMMd().format(tx.date),
-                          style:
-                              const TextStyle(fontSize: 14, color: Colors.grey),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList()),
+           TransactionList(),
           ],
         ),
       ),
