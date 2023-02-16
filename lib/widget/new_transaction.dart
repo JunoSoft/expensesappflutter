7733200610deba68551772b56cc41e47c5bd8535
Junosoft <junosoftltd@gmail.com>
2023-02-16 10:15:37 +0200

    updated

--- lib/widget/new_transaction.dart
+++ lib/widget/new_transaction.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class NewTrsansaction extends StatefulWidget {
-  const NewTrsansaction({super.key});
+class NewTransaction extends StatelessWidget {
+  const NewTransaction({super.key});
 
-  @override
-  State<NewTrsansaction> createState() => _NewTrsansactionState();
-}
-
-class _NewTrsansactionState extends State<NewTrsansaction> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
