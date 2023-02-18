54f9a2e58759dd468488014af6eb27a6e03d86ae
Junosoft <junosoftltd@gmail.com>
2023-02-18 23:32:12 +0200

    added chartbar as statefull

--- lib/widget/chartBar.dart
+++ lib/widget/chartBar.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class ChartBar extends StatelessWidget {
+class ChartBar extends StatefulWidget {
   const ChartBar({super.key});
 
   @override
+  State<ChartBar> createState() => _ChartBarState();
+}
+
+class _ChartBarState extends State<ChartBar> {
+  @override
   Widget build(BuildContext context) {
-    return Column(
-     
-    );
+    return const Placeholder();
   }
 }
