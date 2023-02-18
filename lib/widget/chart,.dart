77c878bfd74b0a06976b2fdfa6f63ee0401f1b29
Junosoft <junosoftltd@gmail.com>
2023-02-18 19:27:55 +0200

    changes

--- lib/widget/chart,.dart
+++ lib/widget/chart,.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+
 
 class Chart extends StatelessWidget {
   const Chart({super.key});
@@ -9,7 +8,7 @@ class Chart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
-      margin: EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
       child: Row(),
     );
   }
