c62349599e653739936d56050140e8fc625773e6
Junosoft <junosoftltd@gmail.com>
2023-02-20 20:10:25 +0200

    added ListTile

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -31,42 +31,23 @@ class TransactionList extends StatelessWidget {
             : ListView.builder(
                 itemCount: transactionlist.length,
                 itemBuilder: (context, index) => Card(
-                      child: Row(
-                        children: <Widget>[
-                          Container(
-                            margin: const EdgeInsets.symmetric(
-                                vertical: 8, horizontal: 10),
-                            padding: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                              color: Theme.of(context).primaryColor,
-                              width: 3,
-                            )),
-                            child: Text('\$ ${transactionlist[index].amount}'),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Container(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 10),
+                      elevation: 5,
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 30,
+                          child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: FittedBox(
                                   child: Text(
-                                    transactionlist[index].title,
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 19,
-                                        color:
-                                            Color.fromARGB(255, 129, 134, 1)),
-                                  )),
-                              Text(
-                                DateFormat.yMMMMd()
-                                    .format(transactionlist[index].date),
-                                style: const TextStyle(
-                                    fontSize: 14, color: Colors.grey),
-                              )
-                            ],
-                          )
-                        ],
+                                      '\$ ${transactionlist[index].amount}'))),
+                        ),
+                        title: Text(
+                          transactionlist[index].title,
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 129, 134, 1)),
+                        ),
+                        subtitle: Text(DateFormat.yMMMMd()
+                            .format(transactionlist[index].date)),
                       ),
                     )));
   }
